56f84aebd9cf25e46287dcdc809681d832e842ed
Amir Ben Shimol <amir@amirsmacbook.com>
2022-01-23 11:36:52 +0200

    ✅ temporary fixed

--- src/components/pages/Main/containers/RecommenderApp/RecommenderApp.view.tsx
+++ src/components/pages/Main/containers/RecommenderApp/RecommenderApp.view.tsx
@@ -6,8 +6,7 @@ import appStoreLogo from '../../../../../assets/images/app-store.png';
 import brandLogo from '../../../../../assets/images/brand-logo.png';
 import firstPhone from '../../../../../assets/images/first-phone.png';
 import secondPhone from '../../../../../assets/images/second-phone.png';
-import groupPhoneIpad from '../../../../../assets/images/group-phone-ipad.png';
-import groupPhoneMobile from '../../../../../assets/images/group-Phone-mobile.png';
+import groupPhoneIpad from '../../../../../assets/images/group-phones.png';
 
 import classes from './RecommenderApp.module.scss';
 
@@ -43,9 +42,8 @@ const RecommenderAppView: React.FC<IProps> = () => {
 				</div>
 				<img className={classes['recommenderAppSectionInner__firstPhone']} src={firstPhone} alt="first phone" />
 				<img className={classes['recommenderAppSectionInner__secondPhone']} src={secondPhone} alt="second phone" />
-				<img className={classes['recommenderAppSectionInner__groupPhone-ipad']} src={groupPhoneIpad} alt="group of phones" />
-				<img className={classes['recommenderAppSectionInner__groupPhone-mobile']} src={groupPhoneMobile} alt="group of phones" />
 			</div>
+			<img className={classes['recommenderAppSection__groupPhone']} src={groupPhoneIpad} alt="group of phones" />
 		</section>
 	);
 };
