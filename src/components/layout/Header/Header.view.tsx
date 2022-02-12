3a9d6f726582b24bc0f222f06f0324ca5a30866e
Tal Rofe <dev@vinyldepository.com>
2022-02-12 21:48:52 +0200

    fix: 🐞 import order
    import order

--- src/components/layout/Header/Header.view.tsx
+++ src/components/layout/Header/Header.view.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Image from 'next/image';
 import Link from 'next/link';
+import Image from 'next/image';
 import { useTranslation } from 'react-i18next';
 
 import brandLogo from '@/images/brand-logo.png';
