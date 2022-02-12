0b6d3e039f79ef7baa0aa5f7de5ed6c8f4e02ee7
Tal Rofe <dev@vinyldepository.com>
2022-02-12 12:25:55 +0200

    refactor: 💡 Added stylelint rules
    Added stylelint rules

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,12 +1,17 @@
 module.exports = {
 	extends: [
-		'stylelint-config-standard',
 		'stylelint-config-standard-scss',
-		'stylelint-config-prettier',
 		'stylelint-config-prettier-scss',
+		'stylelint-config-recess-order',
 	],
 	plugins: ['stylelint-scss', 'stylelint-order'],
 	rules: {
-		'scss/at-import-partial-extension': false,
+		'selector-class-pattern': [
+			'^[a-z][A-Za-z0-9]*((--([a-z][A-Za-z0-9]*)(__([a-z][A-Za-z0-9]*))?)|(__([a-z][A-Za-z0-9]*)(--([a-z][A-Za-z0-9]*))?))?$',
+			{ resolveNestedSelectors: true, message: 'Expected class selector to be camel case' },
+		],
+		'value-no-vendor-prefix': null,
+
+		'scss/at-import-partial-extension': null,
 	},
 };
