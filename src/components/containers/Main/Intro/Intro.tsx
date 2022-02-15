4000b5379ca99bb573b9efdfb29b67d9894535bb
Amir Ben Shimol <amir@AmirsMacBook.local>
2022-02-15 19:22:04 +0200

    fix: 🐞 add email validation & changed images to cropped version
    add email validation & changed images to cropped version

--- src/components/containers/Main/Intro/Intro.tsx
+++ src/components/containers/Main/Intro/Intro.tsx
@@ -1,4 +1,8 @@
-import React from 'react';
+// import { match } from 'assert';
+import React, { useState } from 'react';
+// import axios from 'axios';
+
+import { validateEmail } from '@/utils/validators';
 
 import IntroView from './Intro.view';
 
@@ -7,7 +11,50 @@ interface IProps {
 }
 
 const Intro: React.FC<IProps> = (props: React.PropsWithChildren<IProps>) => {
-	return <IntroView joinersCount={props.joinersCount} />;
+	const [emailInputState, setEmailInputState] = useState<string | null>(null);
+
+	const emailInputChangeHandler = (input: string) => {
+		setEmailInputState(() => input);
+	};
+
+	const [emailInputIsValidState, setEmailInputIsValidState] = useState<boolean>(false);
+
+	const [emailInputTouchedState, setEmailInputTouchedState] = useState<boolean>(false);
+
+	const emailInputIsInValid = !emailInputIsValidState && emailInputTouchedState;
+
+	const formSubmitHandler = (event: KeyboardEvent) => {
+		event.preventDefault();
+
+		setEmailInputTouchedState(true);
+
+		if (emailInputState === null) {
+			return;
+		}
+
+		if (validateEmail(emailInputState)) {
+			// try {
+			// 	const response = await axios.post('')
+
+			// } catch (error) {
+
+			// }
+			setEmailInputIsValidState(() => true);
+			setEmailInputState(() => null);
+		} else {
+			setEmailInputIsValidState(() => false);
+		}
+	};
+
+	return (
+		<IntroView
+			joinersCount={props.joinersCount}
+			emailInputChangeHandler={emailInputChangeHandler}
+			formSubmitHandler={formSubmitHandler}
+			emailInputState={emailInputState}
+			emailInputIsInValid={emailInputIsInValid}
+		/>
+	);
 };
 
 Intro.displayName = 'Intro';
