a798b4f72ba6b1716385c443ec98b86cd84c8b07
Tal Rofe <dev@vinyldepository.com>
2022-02-16 00:27:07 +0200

    refactor: 💡 refactoring http hook
    refactoring http hook

--- src/hooks/http.ts
+++ src/hooks/http.ts
@@ -1,46 +1,77 @@
-import { useState, useEffect } from 'react';
-import axios, { AxiosResponse, AxiosError } from 'axios';
+// * Hook logic is inspired by https://stackoverflow.com/questions/63118511/custom-http-hook-using-axioshttps://stackoverflow.com/questions/63118511/custom-http-hook-using-axios
+
+import { useState } from 'react';
+import axios, { AxiosResponse, AxiosError, AxiosRequestHeaders, Method } from 'axios';
 
 interface IHTTPResponeData {}
 
-type HttpMethod = 'get' | 'post';
+interface IHookResponse<T extends IHTTPResponeData> {
+	response: AxiosResponse<T> | null;
+	error: AxiosError | null;
+	loading: boolean;
+	request: () => Promise<() => void>;
+}
 
+/**
+ * The hook receives the request configuration and returns an object to use
+ * @param url the url to send the request to
+ * @param method the method to use
+ * @param body the request body
+ * @param headers the request headers
+ * @param timeout the request timeout
+ * @returns an object of response, error, loading state and request to use
+ */
 const useHTTP = <T extends IHTTPResponeData>(
 	url: string,
-	method: HttpMethod,
+	method: Extract<Method, 'GET' | 'POST'>,
 	body?: object,
-	headers?: object,
-): { response: T | null; error: object | null; loading: boolean } => {
-	const [responseState, setResponseState] = useState<T | null>(null);
-	const [errorState, setErrorState] = useState<object | null>(null);
-	const [loadingState, setLoadingState] = useState<boolean>(true);
+	headers?: AxiosRequestHeaders,
+	timeout?: number,
+): IHookResponse<T> => {
+	const HTTPTimeout = timeout ?? 5000;
 
-	const request = () => {
-		let axiosQuery: Promise<AxiosResponse<T>>;
+	const [responseState, setResponseState] = useState<AxiosResponse<T> | null>(null);
+	const [errorState, setErrorState] = useState<AxiosError | null>(null);
+	const [loadingState, setLoadingState] = useState<boolean>(false);
 
-		if (method === 'post') {
-			axiosQuery = axios[method](url, headers, body);
-		} else {
-			axiosQuery = axios[method](url, headers);
+	const request = async () => {
+		setLoadingState(() => true);
+
+		// Get a source in-order to cancel the request when hook is being unmounted
+		const axiosSource = axios.CancelToken.source();
+
+		try {
+			if (method === 'GET') {
+				const response = await axios.get(url, {
+					cancelToken: axiosSource.token,
+					timeout: HTTPTimeout,
+				});
+
+				setResponseState(() => response);
+				setLoadingState(() => false);
+			} else if (method === 'POST') {
+				const response = await axios.post(url, body, {
+					cancelToken: axiosSource.token,
+					timeout: HTTPTimeout,
+					headers,
+				});
+
+				setResponseState(() => response);
+				setLoadingState(() => false);
+			}
+		} catch (error) {
+			// Update the state only if the hook isn't being unmounted
+			if (!axios.isCancel(error)) {
+				setErrorState(() => error as AxiosError);
+				setLoadingState(() => false);
+			}
 		}
 
-		axiosQuery
-			.then((response: AxiosResponse<T>) => {
-				setResponseState(response.data);
-			})
-			.catch((error: AxiosError) => {
-				setErrorState(error);
-			})
-			.finally(() => {
-				setLoadingState(false);
-			});
+		// In case the hook is being unmounted - cancel the request
+		return () => axiosSource.cancel();
 	};
 
-	useEffect(() => {
-		request();
-	}, [method, url, body, headers]);
-
-	return { response: responseState, error: errorState, loading: loadingState };
+	return { response: responseState, error: errorState, loading: loadingState, request };
 };
 
 export default useHTTP;
