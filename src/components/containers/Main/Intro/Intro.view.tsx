9ea785cf79e9ca9483a029a5fca1e6a11a846ecc
Tal Rofe <dev@vinyldepository.com>
2022-02-17 18:58:12 +0200

    fix: 🐞 fix nextjs images issues
    fix nextjs images issues

--- src/components/containers/Main/Intro/Intro.view.tsx
+++ src/components/containers/Main/Intro/Intro.view.tsx
@@ -52,7 +52,6 @@ const IntroView: React.FC<IProps> = (props: React.PropsWithChildren<IProps>) => 
 						alt="Intro image"
 						placeholder="blur"
 						objectFit="contain"
-						layout="fill"
 						priority
 					/>
 				</div>
