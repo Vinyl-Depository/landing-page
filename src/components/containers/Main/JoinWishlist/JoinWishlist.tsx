4a3dcf356292e07e920fd9c3e4d2f4ff7da7faa9
Amir Ben Shimol <amir@AmirsMacBook.lan>
2022-02-17 20:22:33 +0200

    fix: 🐞 add validation to waitlist section
    add validation to waitlist section

--- src/components/containers/Main/JoinWishlist/JoinWishlist.tsx
+++ src/components/containers/Main/JoinWishlist/JoinWishlist.tsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { validateEmail } from '@/utils/validators';
+import { EmailValidation } from '@/models/email';
 
 import JoinWishlistView from './JoinWishlist.view';
 
@@ -7,7 +10,41 @@ interface IProps {
 }
 
 const JoinWishlist: React.FC<IProps> = (props: React.PropsWithChildren<IProps>) => {
-	return <JoinWishlistView joinersCount={props.joinersCount} />;
+	const [emailInputState, setEmailInputState] = useState<string | null>(null);
+	const [emailValidationState, setEmailValidationState] = useState<EmailValidation | null>(null);
+
+	const emailInputChangeHandler = (input: string) => setEmailInputState(() => input);
+
+	const formSubmitHandler = (event: React.FormEvent) => {
+		event.preventDefault();
+
+		if (emailInputState === null) {
+			return;
+		}
+
+		if (emailInputState === '') {
+			return setEmailValidationState(() => EmailValidation.BadInput);
+		}
+
+		// If the email is valid then set a success state and reset the email input
+		if (validateEmail(emailInputState)) {
+			setEmailValidationState(() => EmailValidation.Success);
+			setEmailInputState(() => null);
+
+			return;
+		}
+
+		setEmailValidationState(() => EmailValidation.BadInput);
+	};
+	return (
+		<JoinWishlistView
+			joinersCount={props.joinersCount}
+			emailInputChangeHandler={emailInputChangeHandler}
+			formSubmitHandler={formSubmitHandler}
+			emailInput={emailInputState}
+			emailValidation={emailValidationState}
+		/>
+	);
 };
 
 JoinWishlist.displayName = 'JoinWishlist';
--- src/components/containers/Main/JoinWishlist/JoinWishlist.view.tsx
+++ src/components/containers/Main/JoinWishlist/JoinWishlist.view.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import { Trans, useTranslation } from 'react-i18next';
+import { EmailValidation } from '@/models/email';
 
 import vinylLogo from '@/images/join-whistlist-vinyl-logo.png';
 import emailLogo from '@/images/join-whistlist-emails.png';
@@ -11,10 +12,35 @@ import classes from './JoinWishlist.module.scss';
 
 interface IProps {
 	readonly joinersCount: number;
+	readonly emailInputChangeHandler: (_: string) => void;
+	readonly emailInput: string | null;
+	readonly formSubmitHandler: (event: React.FormEvent) => void;
+	readonly emailValidation: EmailValidation | null;
 }
 
 const JoinWishlistView: React.FC<IProps> = (props: React.PropsWithChildren<IProps>) => {
 	const { t } = useTranslation();
+
+	let validationMessage: JSX.Element | string | null = null;
+	let validationMessageClassName: string | null = null;
+
+	if (props.emailValidation === EmailValidation.Success) {
+		validationMessage = t('intro.successValidationMessage');
+		validationMessageClassName = classes['validationMessageSuccess'] ?? '';
+	} else if (props.emailValidation === EmailValidation.BadInput) {
+		validationMessage = (
+			<>
+				<span className={classes['validationMessageError__firstRow']}>
+					<b>{t('intro.errorValidationMessageFirstRow')}</b>
+				</span>
+				<br />
+				<span className={classes['validationMessageError__secondRow']}>
+					{t('intro.errorValidationMessageSecondRow')}
+				</span>
+			</>
+		);
+		validationMessageClassName = classes['validationMessageError'] ?? '';
+	}
 
 	return (
 		<div className={classes['joinWishlistSectionContainer']}>
@@ -49,18 +75,28 @@ const JoinWishlistView: React.FC<IProps> = (props: React.PropsWithChildren<IProp
 						</span>
 						<Trans i18nKey="joinWishlist.headerSecondPart" />
 					</h2>
-					<form className={classes['joinWishlistForm']}>
+					<form
+						className={classes['joinWishlistForm']}
+						noValidate
+						onSubmit={props.formSubmitHandler}
+					>
 						<input
 							className={classes['joinWishlistForm__input']}
 							type="email"
 							placeholder={t('joinWishlist.formPlaceholder')}
+							value={props.emailInput ?? ''}
+							onChange={({ currentTarget: { value } }) => props.emailInputChangeHandler(value)}
 						/>
 						<button className={classes['joinWishlistForm__submit']} type="submit">
 							{t('joinWishlist.button')}
 						</button>
+
 						<button className={classes['joinWishlistForm__mobileSubmit']} type="submit">
 							<VSvg name="submitButton" />
 						</button>
+						{props.emailValidation && (
+							<span className={validationMessageClassName ?? ''}>{validationMessage}</span>
+						)}
 					</form>
 				</div>
 			</section>
