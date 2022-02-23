377d95d0da5b224788280f81dceb5184483af914
dbar-makor <dbar-makor@outlook.com>
2022-02-23 21:14:54 +0200

    feat: 🔥 Lighthouse added

---
+++ lighthouserc.js
@@ -0,0 +1,16 @@
+module.exports = {
+	ci: {
+		collect: {
+			numberOfRuns: 3,
+			staticDistDir: './.next',
+		},
+		assert: {
+			assertions: {
+				'first-contentful-paint': ['warn', { maxNumericValue: 4000 }],
+			},
+		},
+		upload: {
+			target: 'temporary-public-storage',
+		},
+	},
+};
