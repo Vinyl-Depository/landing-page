5c72ee38ba82b1a95cbb284a26fa19ffe0e18d10
Amir Ben Shimol <amir@AmirsMacBook.local>
2022-03-03 19:15:02 +0200

    refactor: 💡 moved randonNumber func to utils folder
    moved randonNumber func to utils folder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import type { GetStaticProps, NextPage } from 'next';
 
+import { randomNumber } from '@/utils/randomNumber';
+
 import Main from '@/containers/Main';
 
 interface IProps {
@@ -15,10 +17,6 @@ Home.displayName = 'Home';
 Home.defaultProps = {};
 
 export const getStaticProps: GetStaticProps = () => {
-	const randomNumber = (min: number, max: number) => {
-		return Math.floor(Math.random() * (max - min)) + min + 1;
-	};
-
 	const today = new Date();
 	const day = today.getDate();
 	const month = today.getMonth() + 1;
