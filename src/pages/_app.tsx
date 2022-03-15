5dc660fd21ccd0baa47bfbfc64c76c914fc3c3fc
Amir Ben Shimol <amir@amirsmacbook.com>
2022-03-15 17:28:15 +0200

    refactor: 💡 removed unecessary meta tag
    removed unecessary meta tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -28,7 +28,6 @@ function MyApp({ Component, pageProps }: AppProps) {
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 				<meta name="theme-color" content="#000000" />
-				<meta name="og:title" content="Vinyl Depository" />
 
 				<meta property="og:title" content="Vinyl Depository" />
 				<meta property="og:type" content="website" />
