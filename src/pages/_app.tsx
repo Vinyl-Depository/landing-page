3469388157304c7d419b3fad596b427882917526
Amir Ben Shimol <amir@AmirsMacBook.lan>
2022-03-16 18:51:09 +0200

    fix: 🐞 fixed the connection to GTM
    fixed the connection to GTM

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,7 @@
 /* eslint-disable react/destructuring-assignment */
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import React, { useEffect } from 'react';
-import TagManager from 'react-gtm-module';
+import React from 'react';
 import Script from 'next/script';
 
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
@@ -18,10 +17,6 @@ import Footer from '@/layout/Footer';
 import Main from '@/layout/Main';
 
 function MyApp({ Component, pageProps }: AppProps) {
-	useEffect(() => {
-		TagManager.initialize({ gtmId: 'GTM-WSCGQG7' });
-	}, []);
-
 	return (
 		<>
 			<Head>
@@ -62,6 +57,18 @@ function MyApp({ Component, pageProps }: AppProps) {
 						gtag('config', 'G-0DRWY2NPCH');
 						`}
 				</Script>
+
+				<Script strategy="lazyOnload" src="https://www.googletagmanager.com/ns.html?id=GTM-WGS32CD" />
+				<Script strategy="lazyOnload">
+					{`
+						(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+						new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+						j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+						'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+						})(window,document,'script','dataLayer','GTM-WGS32CD')
+						`}
+				</Script>
+
 				<Component {...pageProps} />
 			</Main>
 			<Footer />
