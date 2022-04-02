a9e562b1a907aff3a9c00985facb80b94f5350e6
Tal Rofe <tal@vinyldepository.com>
2022-04-02 12:26:27 +0300

    ci: 🚀 fix commitlint regex and remove scope from Renovate's commits
    fix commitlint regex and remove scope from Renovate's commits

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,5 @@
 const commitRegex =
-	/^(chore|ci|docs|feat|fix|perf|refactor|release|style|test): ((\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff]) )?((\[((((?<!([A-Z]{1,10})-?)[A-Z]+-\d+)))\]) )?(([a-zA-Z0-9]+\s)*[a-zA-Z0-9]+)$/;
+	/^(chore|ci|docs|feat|fix|perf|refactor|release|style|test): ((\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff]) )?((\[((((?<!([A-Z]{1,10})-?)[A-Z]+-\d+)))\]) )?(([^\s]+\s)*[^\s]+)$/;
 
 module.exports = {
 	extends: ['@commitlint/config-conventional'],
