1e4f14dad0fe74402b4cac2ffa1cd3b1022845b2
Tal Rofe <tal@vinyldepository.com>
2022-04-12 19:55:19 +0300

    fix: 🐞 fix sitemap to include www
    missing www in sitemap - led to bad SEO

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-	siteUrl: 'https://vinyldepository.com',
+	siteUrl: 'https://www.vinyldepository.com',
 	generateRobotsTxt: true,
 };
